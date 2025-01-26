fda6072165cb0f9d0cb0403d8c3791d0a920f61d
yadavlalu5252 <yadavlalu5252@gmail.com>
2025-01-26 11:50:43 +0530

    dd

---
+++ src/test.js
@@ -0,0 +1,64 @@
+
+
+const express = require("express");
+const app = express();
+
+// Order of route matter the most
+
+// app.use("/",(req, res) => {
+//     res.send("Hello from server")
+// })
+
+// app.use("/test", (req, res) => {
+//     res.send("app is in testing phase")
+// })
+
+// app.use("/hello", (req, res) => {
+//     res.send("app is saying ki hal chal hello ji!!")
+// })
+
+// app.use("/hello/2", (req, res) => {
+//     res.send("hello hello hello");
+//     // if we run route /hello/2 then also /hello will run
+// })
+
+// app.use("/user", (req,res) => {
+//   res.send("I am the king")
+// })
+
+// app.post("/user", (req, res) => {
+//   res.send("app is sending post request");
+// });
+
+// app.delete("/user", (req, res) => {
+//   res.send("deleted succussfully!!");
+// });
+
+// app.get("/ab+c", (req, res) => {
+//   res.send("We are experimenting!! ")
+// })
+
+
+// Dynamic Routing -- params
+app.get("/user/:userId/:username", (req, res) => {
+  console.log(req.params)
+  const {userId, username} = req.params;
+  console.log(userId)
+  console.log(username)
+  res.send("hii")
+})
+
+// Query parameters
+app.get("/user", (req, res) => {
+  console.log(req.query)
+  const name = req.query.name;
+  const lastnnmae = req.query.lastName;
+  console.log(`name is ${name} and lastname is ${lastnnmae} `)
+  res.send({firstName: "lalu", lastName: "yadav"});
+});
+
+
+
+app.listen(3000, () => {
+  console.log("app listen at 3000...");
+});
