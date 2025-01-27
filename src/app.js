b4e830ff7a3792da2f5ce30918340dd3f883d1ac
yadavlalu5252 <yadavlalu5252@gmail.com>
2025-01-27 09:58:07 +0530

    models created

--- src/app.js
+++ src/app.js
@@ -1,65 +1,18 @@
 const express = require("express");
 const app = express();
+const connectDB = require('./config/database')
 
 
-// We can have a multiple request handler inside route
-// app.use("/user", 
-//   (req, res, next) => {
-//     console.log("1st response");
-//     // res.send("Handling the route user 1");
-//     next();
-// },
-// (req, res, next) => {
-//   console.log("2nd Response")
-//   res.send("second Resposne")
-//   // next();
-// }
-// )
 
-// app.use("/user", [rh1, rh2], rh3, rh4)
-// we can add in arrays of request handlers
 
-// we can write separate route handler also of same name
-/* app.use("/user", (req, res, next) => {
-  console.log("First request handler")
-  res.send("I am first");
-  // next();
+
+
+connectDB().then(() => {
+  console.log("Database Connection Established...")
+  app.listen(3000, () => {
+    console.log("app listen at 3000...");
+  });
+}).catch(err => {
+console.log("Database connection failed")
 })
 
-app.use("/user", (req, res, next) => {
-  console.log("second request handler")
-  res.send("I am second");
-}) */
-
-
-  /* Handle Auth middleware for all GET, POST, DELETE... request */
- /*  const {adminAuth} = require("./middlewares/auth")
-  app.use("/admin", adminAuth);
-
-  app.get("/admin/getAllData", (req,res) => {
-    console.log("first");
-    res.send("All data fetched Succussfully!");
-  })
-
-  app.get("/admin/deleteAllData", (req, res) => {
-    res.send("Deleted Succussfully")
-  })
- */
-
-
-
-  // Error handling
-/*   app.get("/admin/getAllData", (req,res) => {
-
-    throw new Error("tujhe error aaya hai! handle kr")
-    res.send("All data fetched Succussfully!");
-  })
-  app.use("/", (err, req, res, next) => {
-    if(err){
-      res.status(500).send("something went wrong")
-    }
-  }) */
-
-app.listen(3000, () => {
-  console.log("app listen at 3000...");
-});
---
+++ src/config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+   await mongoose.connect("mongodb+srv://yadavlalu5252:lalu123@devtinder.can9r.mongodb.net/")
+}
+
+module.exports = connectDB;
