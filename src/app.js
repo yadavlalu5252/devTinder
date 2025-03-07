2da9b14438841b1f8a75fbbd42d395fb3c8eae4f
yadavlalu5252 <yadavlalu5252@gmail.com>
2025-03-07 18:49:58 +0530

    dotenv removed from dependencies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
-require("dotenv").config()
+// require("dotenv").config()
 
 const cors = require("cors");
 app.use(cors({
